0aa5daf534189d70f604ea4a177da8718d58eacb
Nicolás Ortolá <nicolas.ortola@bpmx.com.ar>
2020-07-01 23:32:18 -0300

    code in Error

--- src/services/error.js
+++ src/services/error.js
@@ -1,6 +1,10 @@
-export default (status, msg) => {
-    const err = new Error(msg)
+export default (status, code, msg) => {
+    let message = msg
     if (status)
         err.status = status
+    if (code) {
+        message = `Code: ${code} - Message: ${msg}`
+    }    
+    const err = new Error(message)
     return err
 }
